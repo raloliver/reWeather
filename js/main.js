fbf2dcd8322080310ed833f6bdeea2e81d1e2455
Ral Oliver <raloliver.ws@gmail.com>
2017-03-30 23:42:36 -0300

    Add Files to Project #1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+//dentro de um arquivo JS, o JSON deve estar sempre numa único linha.
+var $userData = '{"Israel":{"userName":"raloliver", "userEmail":"eu@ral.ninja", "userSettings":{"userAdmin":true, "userBeta":true}},"Fernando":{"userName":"fernando", "userEmail":"fe@nando.me", "userSettings":{"userAdmin":false, "userBeta":true}}}';
+var $userList = JSON.parse($userData);
+console.log($userList);
+
+document.querySelector('.msg').innerHTML = $userList.Israel.userEmail;
+*/
+/*
+var $userData = '{"userName":"raloliver", "userEmail":"eu@ral.ninja"}';
+var $userObject = JSON.parse($userData);
+var $userString = JSON.stringify($userObject);
+
+console.log($userObject);
+console.log($userString);
+*/
